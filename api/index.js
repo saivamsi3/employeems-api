b18e00579f4078907ff0ced0b542760014aebc56
sai vamsi <saivamsi358@gmail.com>
2026-06-21 17:33:11 +0530

    ind

--- api/index.js
+++ api/index.js
@@ -1,6 +1,35 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "Backend is working"
-  });
-}
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+import authRouter from './routes/auth.js'
+import departmentRouter from './routes/department.js'
+import employeeRouter from './routes/employee.js'
+import salaryRouter from "./routes/salary.js"
+import dashboardRouter from "./routes/dashboard.js"
+import connectToDatabase from './db/db.js'
+import leaveRouter from "./routes/leave.js"
+import settingRouter from "./routes/setting.js"
+
+await connectToDatabase()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static("public/uploads"))
+app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter)
+app.use('/api/employee', employeeRouter)
+app.use('/api/salary', salaryRouter)
+app.use('/api/leave', leaveRouter)
+app.use('/api/setting', settingRouter)
+app.use('/api/dashboard', dashboardRouter)
+
+app.get("/", (req, res) => {
+  res.send("Employee Management API is running");
+});
+export default app;
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is Running on port ${process.env.PORT}`)
+// })
