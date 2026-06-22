02fd7cb344b641615e45d9335047b267b8538a7f
sai vamsi <saivamsi358@gmail.com>
2026-06-22 20:23:54 +0530

    changed index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(cors({
   origin: [
     // "http://employee-frontend-three.vercel.app",
-    "http://localhost:5000"
+    "http://localhost:5173"
   ],
   credentials:true
 }))
